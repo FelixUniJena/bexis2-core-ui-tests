292828dabf61cf59186c9677a9d27b569db15953
Felix <felix.maximilian.bergmann@uni-jenda.de>
2026-02-12 07:37:26 +0100

    #48 added reload

--- tests/RPM/utils/externalLinkFunctions.js
+++ tests/RPM/utils/externalLinkFunctions.js
@@ -53,19 +53,10 @@ async function checkExternalLink(page, linkName, hasType, hasPrefix, hasURI) {
         await page.waitForLoadState('load');
         await page.waitForTimeout(1500);
         // Check if the save button is enable and reload the page
-        const saveButton = page.locator('button#save').click();
-        await page.waitForTimeout(500);
-
-        await page.waitForSelector('.toast[data-testid=toast] .text-base');
-        const toast = await page.locator('.toast[data-testid=toast]');
-
-        let expectedMessage = 'External link created.';
-        await expect(await toast.locator('.text-base')).toHaveText(expectedMessage);
-        await toast.locator('button').click(); // Close the toast
-        await page.reload();
+        const saveButton = page.locator('button#save');
+        await expect(saveButton).toBeDisabled();
+        await page.reload()
     }
-
-   
 }
 
 async function findExternalLink(page, linkName) {
